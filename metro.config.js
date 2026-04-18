e4d8554108297547a92b7e2cc9836ba7de2773e6
Ahmad Aljandali <ahmadaljandali7@gmail.com>
2026-04-18 12:31:10 +0200

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for Windows absolute paths
+config.resolver.assetExts.push('cjs');
+
 module.exports = config;
